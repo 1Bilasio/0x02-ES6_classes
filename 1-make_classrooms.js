338f0db623e2cb97f943b5a0a49e700bc43950bc
1Bilasio <sirbilasio@gmail.com>
2024-12-07 14:55:00 +0300

    Correction: 0x02. ES6 classes

--- 1-make_classrooms.js
+++ 1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  const newList = [class1, class2, class3];
-  return (newList);
+  const first = new ClassRoom(19);
+  const second = new ClassRoom(20);
+  const third = new ClassRoom(34);
+
+  return [first, second, third];
 }
